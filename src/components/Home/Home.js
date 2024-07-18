2ace77c306330d516f4f96ff3e89aa4aa3af606a
DianaCose <92103409+DianaCose@users.noreply.github.com>
2024-07-18 11:40:14 +0300

    Layout before graphs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,13 @@ import newClient from '../../assets/newClient.svg';
 import earnings from '../../assets/earnings.svg';
 import avatar from '../../assets/avatar.svg';
 import location from '../../assets/location.svg';
+import onTrack from '../../assets/onTrack.svg';
+import arrow_up from '../../assets/arrow_up.svg';
+import transport from '../../assets/transport.svg';
+import grocery from '../../assets/grocery.svg';
+import subscription from '../../assets/subscription.svg';
+import arrow_back from '../../assets/arrow_back.svg';
+import rectangle from '../../assets/rectangle.svg';
 
 export const Home = () => {
 	return (
@@ -35,7 +42,6 @@ export const Home = () => {
 							</div>
 						</SmallChart>
 					</div>
-
 					<SmallChart>
 						<div className="new_client">
 							<img src={newClient} alt="clients icon" />
@@ -63,7 +69,6 @@ export const Home = () => {
 							</div>
 						</div>
 					</SmallChart>
-
 					<div className="activity">
 						<div className="chart_container">
 							<Typography
@@ -75,10 +80,9 @@ export const Home = () => {
 						</div>
 					</div>
 				</div>
-
 				<div className="wrapper">
 					<LargeChart>
-						<div className="chart_container ">
+						<div className="chart_container">
 							<Typography
 								label="Total Spent"
 								text="$682.5"
@@ -87,7 +91,6 @@ export const Home = () => {
 							/>
 						</div>
 					</LargeChart>
-
 					<MediumChart>
 						<div className="user_chart">
 							<img src={avatar} alt="avatar" />
@@ -131,17 +134,121 @@ export const Home = () => {
 						</div>
 					</MediumChart>
 				</div>
-
 				<div className="wrapper">
-					<div className="seven">
-						<MediumChart />
-					</div>
-					<div className="eight">
-						<MediumChart />
-					</div>
-					<div className="nine">
-						<MediumChart />
-					</div>
+					<MediumChart>
+						<div className="track_chart">
+							<div className="chart_container">
+								<Typography
+									label="Spent this month"
+									text="$682.5"
+									classLabel="small_label"
+									classText="text"
+								/>
+								<div className="onTrack">
+									<img src={onTrack} alt="on track" />
+									<div>On track</div>
+								</div>
+							</div>
+							<div className="percentage">
+								<img src={arrow_up} alt="arrow_up" />
+								<div>+2.45%</div>
+							</div>
+						</div>
+					</MediumChart>
+					<MediumChart>
+						<div className="chart_container">
+							<div className="transaction_title">Your transactions</div>
+							<div className="transaction">
+								<img src={transport} alt="transport" />
+								<div>
+									<Typography
+										label="Public Transport"
+										text="22 September 2020"
+										classLabel="medium_text"
+										classText="small_label"
+									/>
+								</div>
+							</div>
+							<div className="transaction">
+								<img src={grocery} alt="grocery" />
+								<div>
+									<Typography
+										label="Grocery Store"
+										text="18 September 2020"
+										classLabel="medium_text"
+										classText="small_label"
+									/>
+								</div>
+							</div>
+							<div className="transaction">
+								<img src={subscription} alt="subscription" />
+								<div>
+									<Typography
+										label="Public Transport"
+										text="22 September 2020"
+										classLabel="medium_text"
+										classText="small_label"
+									/>
+								</div>
+							</div>
+							<div className="back_container ">
+								<a href="#" className="view_all">
+									View all
+								</a>
+								<img src={arrow_back} alt="arrow-back" />
+							</div>
+						</div>
+					</MediumChart>
+					<MediumChart>
+						<div className="chart_container">
+							<div className="Home__header_title">27 May</div>
+							<div className="calendar">
+								<div className="calendar_container">
+									<img src={rectangle} alt="rectangle" />
+									<div>
+										<Typography
+											label="Public Transport"
+											text="22 September 2020"
+											classLabel="medium_text"
+											classText="small_label"
+										/>
+									</div>
+								</div>
+							</div>
+							<div className="calendar">
+								<div className="calendar_container">
+									<img src={rectangle} alt="rectangle" />
+									<div>
+										<Typography
+											label="Grocery Store"
+											text="18 September 2020"
+											classLabel="medium_text"
+											classText="small_label"
+										/>
+									</div>
+								</div>
+							</div>
+							<div className="calendar">
+								<div className="calendar_container">
+									<img src={rectangle} alt="rectangle" />
+									<div>
+										<Typography
+											label="Public Transport"
+											text="22 September 2020"
+											classLabel="medium_text"
+											classText="small_label"
+										/>
+									</div>
+								</div>
+							</div>
+							<div className="back_container ">
+								<a href="#" className="view_all">
+									View all Tasks
+								</a>
+								<img src={arrow_back} alt="arrow-back" />
+							</div>
+						</div>
+					</MediumChart>
 				</div>
 			</div>
 		</div>
